2c6fc1238843bf05ef77af44aa43aa067de2a88e
Karol Boś <webkarolbos@gmail.com>
2023-01-09 10:24:50 +0100

    Create book record UI

--- client/src/CreateBook.jsx
+++ client/src/CreateBook.jsx
@@ -1,23 +1,88 @@
-import React from 'react'
+import { useState, React } from 'react'
 
 export default function CreateBook() {
+  const defaultFormState = {
+    name: "",
+    author: "",
+    rating: "5",
+    genres: [""]
+  };
+  const [formData, setFormData] = useState(defaultFormState);
 
-  function addNewBook() {
+  function handleInput(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    setFormData((data) => { return { ...data, [name]: value } });
+  }
+
+  function addNewBook(event) {
+    event.preventDefault();
     fetch("http://localhost:5050/books", {
       method: "POST",
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ 
-        name: "New Story",
-        author: "Me",
-        rating: 9,
-        genres: ["Comedy"]
+        name: formData.name,
+        author: formData.author,
+        rating: parseInt(formData.rating),
+        genres: formData.genres.split(",")
        })
     })
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+      if (data.success) {
+        alert("Success");
+        setFormData(defaultFormState);
+      } else {
+        alert("Something went wrong, try again later.")
+      }
+    })
   }
 
   return (
-    <button onClick={addNewBook}>Add new book</button>
+    <form>
+      <span>Book record creation</span>
+      <label htmlFor="title">Title</label>
+      <input
+        type="text"
+        id="title"
+        placeholder="My story"
+        onChange={handleInput}
+        name="name"
+        value={formData.name}
+      />
+      <label htmlFor="author">Author</label>
+      <input
+        type="text"
+        id="author"
+        placeholder="Me"
+        onChange={handleInput}
+        name="author"
+        value={formData.author}
+      />
+      <label htmlFor="rating">Rating</label>
+      <div className='range-container'>
+        <input
+          type="range"
+          id="rating"
+          onChange={handleInput}
+          name="rating"
+          value={formData.rating}
+          min="1"
+          max="10"
+        />
+        <div>{formData.rating}</div>
+      </div>
+      <label htmlFor="genres">Genres</label>
+      <input
+        type="text"
+        id="genres"
+        placeholder="Comedy,Fiction,Fantasy"
+        onChange={handleInput}
+        name="genres"
+        value={formData.genres}
+      />
+      <button onClick={addNewBook}>Add new book</button>
+    </form>
   );
 }
