25ab9814691c7532a2eaf26c84845e4eb450d713
Karol Boś <webkarolbos@gmail.com>
2023-01-09 10:12:01 +0100

    Select and delete books UI

--- client/src/Book.jsx
+++ client/src/Book.jsx
@@ -3,10 +3,10 @@ import { React } from 'react'
 export default function Book(props) {
     const maxRating = 10;
     return (
-       <div className='book'>
+       <div className={`book ${props.isSelected ? "selected-border" : "default-border"}`} onClick={props.handleClick}>
             <span>{props.name}</span>
-            <span className='book-author'>by {props.author}</span>
-            <span className='book-author'>&#9733; {props.rating}/{maxRating}</span>
+            <span className='book-data'>by {props.author}</span>
+            <span className='book-data'>&#9733; {props.rating}/{maxRating}</span>
        </div>
     );
 }
